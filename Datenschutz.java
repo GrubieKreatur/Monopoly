4c4fbdfa78980ce8507eff444682d3188e755a34
GrubieKreatur <121688796+GrubieKreatur@users.noreply.github.com>
2023-01-10 10:50:13 +0100

    Main

---
+++ Datenschutz.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Datenschutz here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Datenschutz extends Actor
+{
+    /**
+     * Act - do whatever the Datenschutz wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+     public Datenschutz(){
+        System.out.print("Datenschutz erschaffen");
+    }
+}
